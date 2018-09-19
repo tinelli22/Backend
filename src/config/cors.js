19b1e5aaa3e21135317c70912e35154216c3f3cb
tinelli22 <michaelrtinelli@gmail.com>
2018-09-19 12:55:27 -0300

    afterAuth

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, resp, next) => {
     resp.header('Access-Control-Allow-Origin', '*')
     resp.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    resp.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    resp.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()//passe pro próximo middleware
 }
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,22 @@
 const express = require('express')
-
+const auth = require('./auth')
 
 module.exports = function (server) {
     
-    //Definir url base
-    const router = express.Router()
-    server.use('/api', router)
+   const protectedApi = express.Router()
+   server.use('/api', protectedApi)
 
-    //Rotas de ciclo de pagamento
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+   protectedApi.use(auth)
+
+   const BillingCycle = require('../api/billingCycle/billingCycleService')
+   BillingCycle.register(protectedApi, '/billingCycles')
+
+   // Api publica
+   const openApi = express.Router()
+   server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
